5aece77e2589957d75cbc1783a24d0efa23da8ea
nicothebrush <nicola.riolini@gmail.com>
2020-03-24 14:32:30 +0100

    add skype call

--- counseling/models/counseling.py
+++ counseling/models/counseling.py
@@ -29,6 +29,39 @@ class MedicalPatient(models.Model):
     
     _inherit = 'medical.patient'
     
+    # Button events:
+    # TODO 
+    # Add: skype:username?add
+    # View profile: skype:username?userinfo
+    # Leave a voicemail: skype:username?voicemail
+    # Send file: skype:username?sendfile
+    
+    @api.model
+    def skype_url(self, mode='call'):
+        """ Call skype URL
+            skype:username?<mode>
+        """
+        return {
+            'name': 'Skype mode: %s' % mode
+            'res_model': 'ir.actions.act_url',
+            'type': 'ir.actions.act_url',
+            'target': 'self',
+            'url': 'skype:%s?%s' % (self.skype_name, mode),
+            }
+
+    @api.multi
+    def call_skype(self):
+        """ Call skype call link
+        """
+        return self.skype_url('call')
+        
+    @api.multi
+    def chat_skype(self):
+        """ Call skype chat link
+        """
+        return self.skype_url('chat')
+
+    # Columns:
     skype_name = fields.Char('Skype name', size=64)
     telegram_name = fields.Char('Telegram name', size=64)
     hangout_name = fields.Char('Hangout name', size=64)
